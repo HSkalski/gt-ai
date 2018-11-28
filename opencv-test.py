64b83c6ae1f4f64e524f8f1ad30c705c6cedf420
HSkalski <harrison.skalski@gmail.com>
2018-11-28 16:31:54 -0600

    adds dt calc

--- opencv-test.py
+++ opencv-test.py
@@ -2,16 +2,24 @@ import cv2
 import numpy as np 
 import mss 
 
+import time
+
+
 def record():
     
     mon = {"top": 40, "left": 0, "width": 800, "height": 640}
 
     sct = mss.mss()
-
+    last_time = time.time()
     while 1:
+
         img = np.asarray(sct.grab(mon))
 
-        cv2.imshow('Test',img)
+        dt = time.time() - last_time
+        print('test')
+
+        last_time = time.time()
+        cv2.imshow("test",img)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
