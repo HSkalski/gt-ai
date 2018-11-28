23f0e1f36c2aa60c2e16d69cabfadb866c7638a4
HSkalski <harrison.skalski@gmail.com>
2018-11-28 16:24:10 -0600

    add working screen capture

--- opencv-test.py
+++ opencv-test.py
@@ -1,3 +1,19 @@
-print("Hello world")
+import cv2
+import numpy as np 
+import mss 
 
-#test comment
+def record():
+    
+    mon = {"top": 40, "left": 0, "width": 800, "height": 640}
+
+    sct = mss.mss()
+
+    while 1:
+        img = np.asarray(sct.grab(mon))
+
+        cv2.imshow('Test',img)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+record()
