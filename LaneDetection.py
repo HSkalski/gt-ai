1340856ada21089d6a9c6ab59b6cd3ba47c34fe4
HSkalski <harrison.skalski@gmail.com>
2018-12-03 17:06:22 -0600

    add contour and canny detection

--- LaneDetection.py
+++ LaneDetection.py
@@ -9,7 +9,8 @@ def main():
     while 1:
         img = screen_grab()
         processedImg = process_img(img)
-        cv2.imshow("Lane Detection", img)
+        cv2.imshow("Original IMG", img)
+        #cv2.imshow("Processed IMG", processedImg)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
@@ -21,14 +22,21 @@ def screen_grab():
 
 
 def process_img(img):
-    processedImg = img
+    #processedImg = img
     
     #Set region of intrest
     
 
     #Contour Scan
+    imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(imgray,127,255,0)
+    img2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow("Contours", img2)
+    #Line processing?
+    edges = cv2.Canny(img2,50,150)
+    cv2.imshow("Canny Edges", edges)
 
-
-    return processedImg
+    #return processedImg
+    return img
 
 main()
