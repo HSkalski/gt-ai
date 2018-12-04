b5183e476b97ab9d0c7cc18c21220bf2a95e681d
HSkalski <harrison.skalski@gmail.com>
2018-12-03 18:44:59 -0600

    adds testing script for processing effects

---
+++ ImgProcessing.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np 
+import mss 
+
+monitor = {"top": 40, "left": 0, "width": 800, "height": 640}
+sct = mss.mss()
+
+def main():
+    while 1:
+        img = cv2.imread('gtaStill.jpg')
+        img = cv2.resize(img,(800,600),interpolation = cv2.INTER_CUBIC)
+
+        processedImg = process_img(img)
+        cv2.imshow("Original IMG", img)
+        #cv2.imshow("Processed IMG", processedImg)
+        if cv2.waitKey(25) & 0xFF == ord('q'): #if pressing q 
+            cv2.destroyAllWindows()
+            break
+
+
+def process_img(img):
+    #processedImg = img
+    
+    #Set region of intrest
+    
+
+    #Contour Scan
+    imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret,thresh = cv2.threshold(imgray,127,255,0)
+    imgcontour, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow("Contours", imgcontour)
+
+    #Canny Scan
+    imgedges = cv2.Canny(imgcontour,50,150)
+    cv2.imshow("Canny Edges", imgedges)
+
+    #Line Processing?
+
+
+    #return processedImg
+    return img
+
+main()
